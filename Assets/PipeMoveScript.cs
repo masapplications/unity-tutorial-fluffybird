939540e0809e52bbe8bc89d9b11cc190b337726e
masapplications <masapplications@gmail.com>
2024-08-19 17:01:05 -0700

    adding to debug log

--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -19,6 +19,7 @@ public class PipeMoveScript : MonoBehaviour
 
         if (transform.position.x < deadZone)
         {
+            Debug.Log("Pipe Deleted");
             Destroy(gameObject);
         }
     }
